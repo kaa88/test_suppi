0e37a79b09e71c53502f399bc485750ea33a65d3
Andrei <dre021088@gmail.com>
2024-02-11 20:49:47 +0700

    fix token

--- src/api/const.ts
+++ src/api/const.ts
@@ -1,1 +1,2 @@
 export const API_PATH = "https://api.github.com/graphql";
+export const TOKEN = "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";
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,9 @@
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-import { API_PATH } from "./api/const";
+import { API_PATH, TOKEN } from "./api/const";
 
 import "./index.scss";
-
-const TOKEN = process.env.REACT_APP_TOKEN;
 
 const client = new ApolloClient({
   uri: API_PATH,
